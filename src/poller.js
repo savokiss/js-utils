d4572597ea84c0e90a2bf3957dd0f8eb7d7b7669
savokiss <jaynaruto@qq.com>
2018-07-05 15:19:33 +0800

    chore: eslint

--- src/poller.js
+++ src/poller.js
@@ -1,9 +1,9 @@
 // backoff polling
 // using setTimeout insteadof setInterval
 // inspired by https://blog.github.com/2009-07-30-smart-js-polling/
-export function poller(callback, wait = 3000, multiplier = 1) {
-  ;(function startPoller() {
-    setTimeout(function() {
+export function poller (callback, wait = 3000, multiplier = 1) {
+  ;(function startPoller () {
+    setTimeout(function () {
       callback.call(this, startPoller)
     }, wait)
     wait = wait * multiplier
